ead2c4b9624f72bbaf4292632c604e9bb07dc9d9
amausagi <amausagiii@gmail.com>
2018-06-18 19:35:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# YTL Notification
+## 概要
+- YouTube Liveの配信開始通知をトリガーにAPIコールを行います
+- [HLS Bridge](https://github.com/yukimochi/VRC_HLS)にYouTube LiveのURLを送ります
+- Discordに配信情報を送ります
+- Google Apps Scriptsに利用してサーバーレスかつ無料で運用できます
+- Googleアカウントが必要となります
+
+## 使い方
+1. 専用のGoogleアカウントを作成
+1. [YouTube](https://www.youtube.com/)にて任意のチャンネルを登録し、通知をオン
+1. [通知設定](https://www.youtube.com/account_notifications)にて通知を受け取る方法にメールを指定
+1. [Google Apps Script](https://script.google.com/home)に新規スクリプトを追加し、YTL_notification.gsをコピペ
+1. 実行して各種権限を許可
+1. トリガーを毎分に設定
+
+## ライセンス
+Copyright (c) 2018 amausagi
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
---
+++ YTL_Notification.js
@@ -0,0 +1,60 @@
+function YTL_Notification() {
+  var hls_bridge_endopint = 'https://vrchat-hls-bridge.herokuapp.com/submit';
+  var discord_endpoint = '--discord api endpoint url goes here--';
+  var instance_key = '--instance key goes here--'
+  
+  var start = 0;
+  var max = 500;
+  var threads = GmailApp.search('from:YouTube is:unread', start, max);
+  
+  for (var n in threads) {
+    threads[n].markRead();
+  }
+  var the = threads[0];
+  if(threads.length !== 0){
+    var msgs = the.getMessages();
+    var msg = msgs[0];
+    var url = msg.getBody().match(/watch%3Fv%3D[\w?=%&;]+%26/);
+    var subject = msg.getSubject();
+    var channel = subject.match(/🔴 (.*) はライブ配信中です/);
+    
+    if( channel !== null){
+      var channel = channel[0]
+      .replace(/🔴 /, '')
+      .replace(/ はライブ配信中です/, '');
+      var title = subject.match(/はライブ配信中です: (.*)/)[0]
+      .replace(/はライブ配信中です: /, '');
+      if(url !== null){
+        url = url[0]
+        .replace(/watch%3Fv%3D/,'')
+        .replace(/%26/,'');
+        url = 'https://www.youtube.com/watch?v=' + url;
+      }
+      var post = '📡Live on ' + channel + ': ' + title + '\n' + url;
+      
+      //HLS Bridge
+      var data = {
+        'instance' : instance_key,
+        'ls_url' : url,
+        'override' : '0',
+      };
+      var options = {
+        'method': 'post',
+        'contentType': 'application/json',
+        'payload': JSON.stringify(data),
+      };
+      var response = UrlFetchApp.fetch(hls_bridge_endopint, options);
+      
+      //Discord
+      var data = {
+        'content' : post,
+      };
+      var options = {
+        'method': 'post',
+        'contentType': 'application/json',
+        'payload': JSON.stringify(data),
+      };
+      var response = UrlFetchApp.fetch(discord_endpoint, options);
+    }
+  }
+}
